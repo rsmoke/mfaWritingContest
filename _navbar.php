cd7b88a6ec6f3a153cfc35778ef66f3fd233824c
Rick Smoke <rsmoke@umich.edu>
2018-08-16 15:38:19 -0400

    UPDATE: fix layout and add question button

--- _navbar.php
+++ _navbar.php
@@ -6,11 +6,13 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav ml-auto">
-        <a class="nav-link dropdown-toggle ml-auto" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Signed in as <?php echo $login_name;?></a>
-        <div class="dropdown-menu dropdown-menu-right text-left" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="detailEdit.php" data-toggle="tooltip" data-placement="left" title="open your profile"><span class="text-info"><i class="fas fa-user-circle"></i></span></a>
-          <a class="dropdown-item" href="https://weblogin.umich.edu/cgi-bin/logout" data-toggle="tooltip" data-placement="left" title="sign-out"><span class="text-danger"><i class="fas fa-sign-out-alt"></i></span></a>
+
+        <a class="nav-link dropdown-toggle ml-auto" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Signed in as: <span style='color:dodgerblue;'><?php echo $login_name;?></span></a>
+        <div class="dropdown-menu dropdown-menu-right text-center" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="detailEdit.php" data-toggle="tooltip" data-placement="left" title="open your profile"><span class="text-info"><i class="fas fa-user-circle fa-2x"></i></span></a>
+          <a class="dropdown-item" href="https://weblogin.umich.edu/cgi-bin/logout" data-toggle="tooltip" data-placement="left" title="sign-out"><span class="text-danger"><i class="fas fa-sign-out-alt fa-2x"></i></span></a>
         </div>
+        <a class="nav-item nav-link active ml-auto bg-primary text-white rounded" href="mailComment.php" data-toggle="tooltip" data-placement="left" title="send us your questions or comments"><i class="fas fa-question fa-lg"></i></a>
       </div>
     </div>
   </nav>
