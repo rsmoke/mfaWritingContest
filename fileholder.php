d5f8639dc06301f3c18f2ecd3e40590d55277466
Rick Smoke <rsmoke@umich.edu>
2018-08-21 18:05:51 -0400

    ADD: setup new uploads folder location

--- fileholder.php
+++ fileholder.php
@@ -1,4 +1,4 @@
 <?php
   header('Content-Type: application/pdf');
-  readfile($_SERVER["DOCUMENT_ROOT"] . '/../contestfiles/' . preg_replace('/[^-a-zA-Z0-9_\.]/', '', $_GET['file']));
+  readfile($_SERVER["DOCUMENT_ROOT"] . '/../mfacontestfiles/' . preg_replace('/[^-a-zA-Z0-9_\.]/', '', $_GET['file']));
 ?>
