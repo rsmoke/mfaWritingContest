3efd03735a8a2b734b1dd0fce4c16fd3aad160d5
Rick Smoke <rsmoke@umich.edu>
2018-08-30 18:08:52 -0400

    ADD: new contest update UI stx

--- contest_edit.php
+++ contest_edit.php
@@ -62,22 +62,22 @@ if ($isAdmin){
       <div class="col">
         <div id="new_contests"> <!-- setup/manage new contests and manage judges for active contests -->
           <?php
-          $sqlAdmSel = <<<SQL
+          $sqlContestsSel = <<<SQL
           SELECT *
           FROM vw_contestlisting
           ORDER BY ContestsName
 SQL;
-          if (!$resADM = $db->query($sqlAdmSel)) {
-          db_fatal_error("data read issue", $db->error, $sqlAdmSel, $login_name);
+          if (!$resADM = $db->query($sqlContestsSel)) {
+          db_fatal_error("data read issue", $db->error, $sqlContestsSel, $login_name);
           exit;
           }
           while ($row = $resADM->fetch_assoc()) {
-            $fullname = ldapGleaner($row['uniqname']);
+            // $fullname = ldapGleaner($row['uniqname']);
             $html = '<div class="record">';
-            $html .= '<form action="#" method="post" >';
-            $html .= '<input type="hidden" name="admin_uniq" value="' . $row['uniqname'] . '" />';
-            $html .= '<strong>' . $fullname[0] . " " . $fullname[1] . '</strong>  -- ' . $row['uniqname'] .
-              '<button type="submit" name="admin_delete" class="m-1 btn btn-sm btn-outline-light"><i class="fas fa-sm fa-trash text-danger"></i></button>';
+            $html .= '<form action="' . htmlspecialchars($_SERVER["PHP_SELF"]) . '" method="post" >';
+            $html .= '<input type="hidden" name="contestid" value="' . $row['contestid'] . '" />';
+            $html .= '<strong>' . $row['ContestsName'] . '</strong>' . ' - ' . $row['date_open'] . ' - ' . $row['date_closed'] .
+              '<button type="submit" name="contest_delete" class="m-1 btn btn-sm btn-outline-light"><i class="fas fa-sm fa-trash text-danger"></i></button>';
             $html .= '</form>';
             $html .= '</div>';
             echo $html;
@@ -92,11 +92,12 @@ SQL;
         <div id="manage_entries"> <!-- manage entries for active contests -->
           <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" >
             <div id="myAdminForm"><!-- add Admin -->
-              To add an Administrator please enter their <b>uniqname</b> below:<br>
-              <input class="form_control" type="text" name="admin_uniq" />
-              <button type="submit"  name="admin_add" class=" m-1 btn btn-info btn-sm" id="adminAdd">Add Administrator</button>
-              <br />
-              <i>--look up uniqnames using the <a href="https://mcommunity.umich.edu/" target="_blank">Mcommunity directory</a>--</i>
+              To add a new set of contests please enter open/close dates and click below:<br>
+              <label for="from">From</label>
+              <input type="text" id="from" name="from">
+              <label for="to">to</label>
+              <input type="text" id="to" name="to">
+              <button type="submit"  name="admin_add" class=" m-1 btn btn-info btn-sm" id="contestAdd">Add contests for <?php echo date('Y');?>/<?php echo date('Y')+1;?></button>
             </div>
             <!-- //////////////////////////////// -->
           </form><!-- add Admin -->
@@ -108,6 +109,39 @@ SQL;
 <?php } else { redirect_to(); }
 
 include("_footer.php"); ?>
+<script src="js/jquery-ui.min"></script>
+<script>
+$( function() {
+  var dateFormat = "mm/dd/yy",
+    from = $( "#from" )
+      .datepicker({
+        // defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 3
+      })
+      .on( "change", function() {
+        to.datepicker( "option", "minDate", getDate( this ) );
+      }),
+    to = $( "#to" ).datepicker({
+      defaultDate: "+3m",
+      changeMonth: true,
+      numberOfMonths: 3
+    })
+    .on( "change", function() {
+      from.datepicker( "option", "maxDate", getDate( this ) );
+    });
 
+  function getDate( element ) {
+    var date;
+    try {
+      date = $.datepicker.parseDate( dateFormat, element.value );
+    } catch( error ) {
+      date = null;
+    }
+
+    return date;
+  }
+} );
+</script>
 </body>
 </html>
