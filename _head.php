be57efef5ca92c3464b82ddc9dda84db2f729b68
Rick Smoke <rsmoke@umich.edu>
2018-09-11 11:07:37 -0400

    ADD: title element to head

--- _head.php
+++ _head.php
@@ -1,7 +1,7 @@
 <head>
   <META http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
-  <title></title>
+  <title><?php echo $contestTitle;?></title>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
